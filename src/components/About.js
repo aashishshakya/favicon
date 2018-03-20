a50da0ed308a72af530abeded5c050a090f96375
aashishshakya <aashishshakya@gmail.com>
2018-03-20 17:59:47 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Favicon from 'react-favicon';
+
+const About = () => (
+  <div>
+    <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico" />
+
+    <h2>About</h2>
+  </div>
+);
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Favicon from 'react-favicon';
+const Home = () => (
+  <div>
+    <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico" />
+
+    <h2>Home</h2>
+  </div>
+);
+
+export default Home;
